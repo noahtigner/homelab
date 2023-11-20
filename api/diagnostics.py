b6daafd9fb81d229c70737393abc35fc95ed4c6b
Noah Tigner <noahzanetigner@gmail.com>
2023-11-19 21:18:39 -0700

    Error Handling for Diagnostics

--- api/diagnostics.py
+++ api/diagnostics.py
@@ -1,10 +1,18 @@
+import os
 import psutil
+from typing import Union
 
-def get_cpu_temp() -> float:
-    f = open('/sys/class/thermal/thermal_zone0/temp')
-    raw_temp = f.readline()
-    f.close()
-    return float(raw_temp) / 1000
+def get_cpu_temp() -> Union[float, None]:
+    temperature_file_path = '/sys/class/thermal/thermal_zone0/temp'
+    try:
+        raw_temp = None
+        with open(temperature_file_path) as f:
+            raw_temp = f.readline().strip('\n')
+        return float(raw_temp) / 1000
+    except (FileNotFoundError, TypeError, ValueError) as e:
+        print(e)
+        print('Could not read CPU temperature')
+        return None
 
 def get_cpu_count() -> int:
     return psutil.cpu_count()
