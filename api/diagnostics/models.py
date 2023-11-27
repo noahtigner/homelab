d0fdfa4a9eace330fddce0ed8f50e0cc858505d0
Noah Tigner <noahzanetigner@gmail.com>
2023-11-26 19:07:42 -0700

    API: created diagnostics models

---
+++ api/diagnostics/models.py
@@ -0,0 +1,21 @@
+from typing import Union
+from pydantic import BaseModel
+
+class CPUModel(BaseModel):
+    count: int
+    percent: list[float]
+    temp: Union[float, None] = None
+
+
+class UsageModel(BaseModel):
+    total: int
+    used: int
+    available: int
+    percent: float
+
+
+class DiagnosticsModel(BaseModel):
+    cpu: CPUModel
+    memory: UsageModel
+    disk: UsageModel
+    pids: list[int]
--- api/diagnostics/router.py
+++ api/diagnostics/router.py
@@ -2,19 +2,18 @@ from typing import Union
 from fastapi import APIRouter, Depends, HTTPException
 
 from api.diagnostics.retrieval import get_cpu_percent, get_cpu_usage, get_mem_usage, get_disk_usage, get_pids
+from api.diagnostics.models import DiagnosticsModel
 
 router = APIRouter(
     prefix='/diagnostics',
     tags=['Diagnostics'],
 )
 
-@router.get("/")
+@router.get("/", response_model=DiagnosticsModel)
 def get_diagnostics(interval: Union[float, None] = None):
-    return {
-        "diagnostics": {
-            "cpu": get_cpu_usage(interval),
-            "memory": get_mem_usage(),
-            "disk": get_disk_usage(),
-            "pids": get_pids(),
-        },
-    }
+    return DiagnosticsModel(
+        cpu=get_cpu_usage(interval),
+        memory=get_mem_usage(),
+        disk=get_disk_usage(),
+        pids=get_pids(),
+    )
