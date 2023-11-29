e759024bf34e44d728d1789cdee66082d12d6397
Noah Tigner <noahzanetigner@gmail.com>
2023-11-29 10:50:11 -0700

    Dashboard: Pihole Attribution

--- dashboard/src/Attribution.tsx
+++ dashboard/src/Attribution.tsx
@@ -1,14 +1,18 @@
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+import piholeLogo from '/pihole.svg';
 
 export default function Attribution() {
   return (
     <div>
       <a href="https://vitejs.dev" target="_blank">
-        <img src={viteLogo} className="logo" alt="Vite logo" />
+        <img src={viteLogo} className="logo vite" alt="Vite logo" />
       </a>
       <a href="https://react.dev" target="_blank">
         <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+      <a href="https://pi-hole.net/" target="_blank">
+        <img src={piholeLogo} className="logo pihole" alt="Pihole logo" />
       </a>
     </div>
   );
