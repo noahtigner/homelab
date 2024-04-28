e67e459ad50c76f0bdf8e9d8e15f094f9efed352
Noah Tigner <noahzanetigner@gmail.com>
2024-04-28 13:10:32 -0600

    Updated env var schema

--- validateEnvVars.js
+++ validateEnvVars.js
@@ -10,6 +10,7 @@ const envSchema = envObject({
 	IP: envString().ip(),
 	LEETCODE_USERNAME: envNonEmptyString(),
 	GITHUB_USERNAME: envNonEmptyString(),
+	GA4_PROPERTY_ID: envNonEmptyString(),
 });
 
 validateEnvVars({ schema: envSchema });
